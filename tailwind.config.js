8c23607c4c4558123d24aad5c855172b04c5e879
unknown <deepeshiitjaiswal786@gmail.com>
2025-01-31 16:23:26 +0530

    Update profile photo and add LinkedIn URL

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,33 @@ export default {
         center: true,
         padding: '1rem',
       },
+      colors: {
+        'mac-purple': '#9F2B68',
+        'mac-blue': '#147EFB',
+        'mac-pink': '#FF2D55',
+        'mac-orange': '#FF9500',
+        'mac-yellow': '#FFCC00',
+        'mac-green': '#28CD41',
+        'mac-gray': '#8E8E93',
+      },
+      animation: {
+        'gradient': 'gradient 15s ease infinite',
+      },
+      keyframes: {
+        gradient: {
+          '0%, 100%': {
+            'background-size': '200% 200%',
+            'background-position': 'left center'
+          },
+          '50%': {
+            'background-size': '200% 200%',
+            'background-position': 'right center'
+          },
+        },
+      },
+      backdropBlur: {
+        'xs': '2px',
+      },
     },
   },
   plugins: [
